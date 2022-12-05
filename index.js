8f83b90ba8f455caf7c0d1112872ccd280ebc0b4
maqiie <99504010+maqiie@users.noreply.github.com>
2022-12-05 22:35:21 +0300

    coplete assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+main. remove();
+
+
+const newHeader = document.createElement("h1");
+// document.body.append(newHeader);
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = 'TRACY is the champion';
+// document.body.append(newHeader);
